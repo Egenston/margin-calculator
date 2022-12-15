07a8b49d0c5314493ad1749d49e916eadf389373
Vlad Hudym <vladyslav.hudym@westcode.de>
2022-12-15 17:59:09 +0100

    Margin calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'margin-calculator';
+  calculatedMargin: number = 0;
+  potentialProfit: number = 0;
+  
+  form = new FormGroup({
+    riskAmount: new FormControl('', [Validators.required, Validators.min(0)]),
+    riskOption: new FormControl('$', [Validators.required, Validators.min(0)]),
+    depositBalance: new FormControl({value: '', disabled: true}, [Validators.required, Validators.min(0)]),
+    creditLeverage: new FormControl('', [Validators.required, Validators.min(0)]),
+    entryPoint: new FormControl('', [Validators.required, Validators.min(0)]),
+    stopLoss: new FormControl('', [Validators.required, Validators.min(0)]),
+    takeProfit: new FormControl('', [Validators.required, Validators.min(0)])
+  })
+
+  checkRiskOption = () => {
+    let riskAmountControls = this.form.controls['riskAmount'];
+    if (this.form.value.riskOption === "%") {
+      riskAmountControls.addValidators(Validators.max(100));
+      riskAmountControls.updateValueAndValidity();
+      this.form.controls['depositBalance'].enable();
+    }
+    else {
+      riskAmountControls.clearValidators();
+      riskAmountControls.setValidators([Validators.required, Validators.min(0)])
+      riskAmountControls.updateValueAndValidity();
+      this.form.controls['depositBalance'].disable();
+    }
+  }
+
+  displayMargin = () => {
+    if (this.form.value.riskOption === "%") this.calculateRiskAmount();
+    else this.calculateMargin(this.form.value.riskAmount);
+  }
+
+  calculateRiskAmount = () => {
+    let depositBalance: number = this.form.value.depositBalance;
+    let riskAmountInPercentages: number = this.form.value.riskAmount;
+    let riskAmount = (depositBalance * riskAmountInPercentages) / 100;
+    this.calculateMargin(riskAmount);
+  }
+
+  calculateMargin = (riskAmount: number) => {
+    let entryPoint: number = this.form.value.entryPoint;
+    let stopLoss: number = this.form.value.stopLoss;
+    let takeProfit: number = this.form.value.takeProfit;
+    let leverage: number = this.form.value.creditLeverage;
+    let stopPriceMovement: number = parseFloat(((Math.abs(entryPoint - stopLoss) * 100) / entryPoint).toFixed(2));
+    let profitPriceMovement: number = parseFloat(((Math.abs(entryPoint - takeProfit) * 100) / entryPoint).toFixed(2));
+    this.calculatedMargin = (riskAmount * 100) / (stopPriceMovement * leverage);
+    this.potentialProfit = (this.calculatedMargin * profitPriceMovement * leverage) / 100;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
